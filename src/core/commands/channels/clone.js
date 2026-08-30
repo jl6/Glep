1d01b1c293b393e10a15eb935c90406a4ff6c0db
gleppie <jcruzcanete@icloud.com>
2026-08-30 15:09:24 -0700

    clone a any channel and all its perms

---
+++ src/core/commands/channels/clone.js
@@ -0,0 +1,42 @@
+const { EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    name: 'clone',
+    description: 'Duplicates the current channel.',
+    
+    async execute(msg, args, client, db) {
+        if (!msg.member.permissions.has(PermissionFlagsBits.ManageChannels)) {
+            return msg.reply('Missing permissions: Manage Channels required.');
+        }
+
+        const ch = msg.channel;
+
+        try {
+            const cloneCh = await ch.clone({
+                reason: `Cloned by ${msg.author.tag}`
+            });
+
+            if (ch.parent) {
+                await cloneCh.setParent(ch.parent, { lockPermissions: false });
+            }
+
+            await cloneCh.setPosition(ch.position);
+
+            const embed = new EmbedBuilder()
+                .setTitle('Channel Cloned')
+                .setDescription('This channel has been duplicated with original permissions and settings.')
+                .setColor(0x2f3136)
+                .addFields(
+                    { name: 'Original', value: `<#${ch.id}>`, inline: true },
+                    { name: 'Clone', value: `${cloneCh}`, inline: true }
+                )
+                .setTimestamp();
+
+            await cloneCh.send({ embeds: [embed] });
+            await msg.reply(`Channel cloned: ${cloneCh}`);
+        } catch (err) {
+            console.error(err);
+            await msg.reply('Failed to clone channel.');
+        }
+    }
+};
