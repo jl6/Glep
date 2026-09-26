c5de11d4105a71c475e4fc65ecfe83020faa7ce3
gleppie <jcruzcanete@icloud.com>
2026-09-25 23:18:02 -0700

    ready.js

--- src/core/events/client/ready.js
+++ src/core/events/client/ready.js
@@ -1,13 +1,19 @@
-const Database = require('better-sqlite3');
-const path = require('path');
-const db = new Database(path.join(process.cwd(), 'database', 'data.db'));
-
-let isInitialized = false;
-
 module.exports = {
     name: 'clientReady',
     once: true,
-    execute(client) {
-        console.log(`Online`);
+    execute(client) { 
+        client.user.setActivity('Prefix :  _', { type: 4 });
+        console.log(`Logged in as ${client.user.tag}`);
     }
 };
+
+/* 
+1 = streaming 
+2 = listening 
+3 = watching
+4 = status 
+
+
+
+
+*/
